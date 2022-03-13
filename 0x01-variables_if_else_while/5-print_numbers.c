5e989e21e25eb528345aca77f6ac23c027641355
Harriet66 <missnarotsoirya@gmail.com>
2022-03-13 11:42:01 +0300

    My 5 commit

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,18 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - Prints the numbers from 0 to 9
  *
  * Return: Always 0(Success)
  */
 int main(void)
 {
 
-		for (int i = 0; i <= 9; i++)
-		{
-			printf("%d", i);
-		}
-		printf("\n");
-
+		printf("0123456789\n");
 	return (0);
 }
