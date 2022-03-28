e5ae6bf5c58328a95603257d010dedcc82e2a107
Harriet66 <missnarotsoirya@gmail.com>
2022-03-29 01:24:36 +0300

    7-print_chessboard

---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints chessboard.
+ * @a: matrix
+ */
+void print_chessboard(char (*a)[8])
+{
+	int k, j;
+
+	for (k = 0; k < 8; k++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(*(*(k + a) + j));
+		}
+		_putchar('\n');
+	}
+}
