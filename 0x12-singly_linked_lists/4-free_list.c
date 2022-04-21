f6252dcdace906373cae962738dd2e3a0a860692
Harriet66 <missnarotsoirya@gmail.com>
2022-04-21 21:19:10 +0300

    4-free_list

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free_list - function to free a linked list
+ * @head: pointer to a list to be freed
+ *
+ * Return: nothing
+ */
+void free_list(list_t *head)
+{
+	list_t *temp = head;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = head->next;
+		free(temp->str);
+		free(temp);
+	}
+}
